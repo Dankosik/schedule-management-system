643699d0da0729c3787ee8c1bf6900543bfe3e2d
dankos <daniil.koryto@gmail.com>
2020-12-24 19:10:58 +1000

    changed Teacher

--- src/main/java/com/foxminded/university/management/schedule/models/Teacher.java
+++ src/main/java/com/foxminded/university/management/schedule/models/Teacher.java
@@ -4,23 +4,14 @@ import java.util.List;
 import java.util.Objects;
 
 public class Teacher extends Person {
-    private List<Lecture> lectures;
-    private List<Student> students;
+    private List<Subject> subjects;
 
-    public List<Lecture> getLectures() {
-        return lectures;
+    public List<Subject> getSubjects() {
+        return subjects;
     }
 
-    public void setLectures(List<Lecture> lectures) {
-        this.lectures = lectures;
-    }
-
-    public List<Student> getStudents() {
-        return students;
-    }
-
-    public void setStudents(List<Student> students) {
-        this.students = students;
+    public void setSubjects(List<Subject> subjects) {
+        this.subjects = subjects;
     }
 
     @Override
@@ -29,11 +20,11 @@ public class Teacher extends Person {
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Teacher teacher = (Teacher) o;
-        return Objects.equals(lectures, teacher.lectures) && Objects.equals(students, teacher.students);
+        return Objects.equals(subjects, teacher.subjects);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), lectures, students);
+        return Objects.hash(super.hashCode(), subjects);
     }
 }
