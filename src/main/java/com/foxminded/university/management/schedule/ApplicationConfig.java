77bf48207442c6a8784a9dd7ad6394d1e07526fe
dankos <daniil.koryto@gmail.com>
2020-12-27 01:58:30 +1000

    added ApplicationConfig

---
+++ src/main/java/com/foxminded/university/management/schedule/ApplicationConfig.java
@@ -0,0 +1,29 @@
+package com.foxminded.university.management.schedule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.foxminded.university.management.schedule")
+@PropertySource("classpath:database.properties")
+public class ApplicationConfig {
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(environment.getProperty("url"));
+        dataSource.setUsername(environment.getProperty("dbuser"));
+        dataSource.setPassword(environment.getProperty("dbpassword"));
+        dataSource.setDriverClassName(environment.getProperty("driver"));
+        return dataSource;
+    }
+}
