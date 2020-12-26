e1a4c8ffe33e9150a44d9c2ca2d3a5ad4aa979d8
dankos <daniil.koryto@gmail.com>
2020-12-27 02:08:01 +1000

    changed fields in Student

--- src/main/java/com/foxminded/university/management/schedule/models/Student.java
+++ src/main/java/com/foxminded/university/management/schedule/models/Student.java
@@ -4,18 +4,15 @@ import java.util.Objects;
 
 public class Student extends Person {
     private Long id;
-    private Long groupId;
-    private Group group;
-    private Faculty faculty;
     private Integer courseNumber;
+    private Long groupId;
+    private Long facultyId;
     private Long universityId;
 
-    @Override
     public Long getId() {
         return id;
     }
 
-    @Override
     public void setId(Long id) {
         this.id = id;
     }
@@ -28,20 +25,12 @@ public class Student extends Person {
         this.groupId = groupId;
     }
 
-    public Group getGroup() {
-        return group;
+    public Long getFacultyId() {
+        return facultyId;
     }
 
-    public void setGroup(Group group) {
-        this.group = group;
-    }
-
-    public Faculty getFaculty() {
-        return faculty;
-    }
-
-    public void setFaculty(Faculty faculty) {
-        this.faculty = faculty;
+    public void setFacultyId(Long facultyId) {
+        this.facultyId = facultyId;
     }
 
     public Integer getCourseNumber() {
@@ -52,27 +41,16 @@ public class Student extends Person {
         this.courseNumber = courseNumber;
     }
 
-    public Long getUniversityId() {
-        return universityId;
-    }
-
-    public void setUniversityId(Long universityId) {
-        this.universityId = universityId;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
         Student student = (Student) o;
-        return Objects.equals(id, student.id) && Objects.equals(groupId, student.groupId) &&
-                Objects.equals(group, student.group) && Objects.equals(faculty, student.faculty) &&
-                Objects.equals(courseNumber, student.courseNumber) && Objects.equals(universityId, student.universityId);
+        return Objects.equals(id, student.id) && Objects.equals(groupId, student.groupId) && Objects.equals(facultyId, student.facultyId) && Objects.equals(courseNumber, student.courseNumber) && Objects.equals(universityId, student.universityId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), id, groupId, group, faculty, courseNumber, universityId);
+        return Objects.hash(id, groupId, facultyId, courseNumber, universityId);
     }
 }
