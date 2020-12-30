6b99caca5fd2e71201535bdc87ae0a19fb03b1d5
dankos <daniil.koryto@gmail.com>
2020-12-31 05:03:14 +1000

    added V1 migration which create tables

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,54 @@
+create table schedule
+(
+
+);
+
+create table university
+(
+
+);
+
+create table audience
+(
+
+);
+
+create table faculties
+(
+
+);
+
+create table departments
+(
+
+);
+
+create table lectures
+(
+
+);
+
+create table groups
+(
+
+);
+
+create table students
+(
+
+);
+
+create table lessons
+(
+
+);
+
+create table subjects
+(
+
+);
+
+create table teachers
+(
+
+);
