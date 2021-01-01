f38fe976a7f282144f643ae82b8e919d9a3b97d1
dankos <daniil.koryto@gmail.com>
2021-01-02 08:31:50 +1000

    changed Application

--- src/main/java/com/foxminded/university/management/schedule/Application.java
+++ src/main/java/com/foxminded/university/management/schedule/Application.java
@@ -1,12 +1,22 @@
 package com.foxminded.university.management.schedule;
 
+import com.foxminded.university.management.schedule.dao.AudienceDao;
+import com.foxminded.university.management.schedule.models.Audience;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class Application {
-
+    @Autowired
+    static DataSource dataSource;
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        Audience audience = new Audience(2, 100, 2L);
+        AudienceDao audienceDao = new AudienceDao(dataSource);
+        audienceDao.save(audience);
     }
 }
