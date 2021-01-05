45b7388473d98a88604349dc0208cae80e483b45
dankos <daniil.koryto@gmail.com>
2021-01-06 02:31:51 +1000

    added mapping form PostgreSQL Interval to Java Duration in LessonRowMapper

--- src/main/java/com/foxminded/university/management/schedule/dao/row_mappers/LessonRowMapper.java
+++ src/main/java/com/foxminded/university/management/schedule/dao/row_mappers/LessonRowMapper.java
@@ -1,6 +1,7 @@
 package com.foxminded.university.management.schedule.dao.row_mappers;
 
 import com.foxminded.university.management.schedule.models.Lesson;
+import org.postgresql.util.PGInterval;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
@@ -15,7 +16,13 @@ public class LessonRowMapper implements RowMapper<Lesson> {
         lesson.setId(resultSet.getLong("id"));
         lesson.setNumber(resultSet.getInt("number"));
         lesson.setStartTime(resultSet.getTime("start_time"));
-        lesson.setDuration(Duration.ofMinutes(resultSet.getLong("duration")));
+
+        PGInterval pgInterval = (PGInterval) resultSet.getObject("duration");
+        int minutes = pgInterval.getMinutes();
+        int hours = pgInterval.getHours();
+        Duration duration = Duration.ofMinutes(hours * 60L + minutes);
+
+        lesson.setDuration(duration);
         lesson.setSubjectId(resultSet.getLong("subject_id"));
         return lesson;
     }
