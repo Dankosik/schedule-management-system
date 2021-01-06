21d6272b0b4001527aff4827d1a13ec4b9b7ff13
dankos <daniil.koryto@gmail.com>
2021-01-07 00:03:30 +1000

    changed adding columns to tables in V3__add_all_other_columns_to_tables.sql

--- src/main/resources/db/migration/V3__add_all_other_columns_to_tables.sql
+++ src/main/resources/db/migration/V3__add_all_other_columns_to_tables.sql
@@ -1,59 +1,57 @@
-ALTER TABLE university
-    ADD COLUMN schedule_id BIGINT NOT NULL REFERENCES schedule (id) ON DELETE CASCADE;
+ALTER TABLE schedule
+    ADD COLUMN university_id BIGINT  REFERENCES university (id) ON DELETE CASCADE;
 
 ALTER TABLE audiences
-    ADD COLUMN number        INT    NOT NULL,
-    ADD COLUMN capacity      INT    NOT NULL,
-    ADD COLUMN university_id BIGINT NOT NULL REFERENCES university (id) ON DELETE CASCADE;
+    ADD COLUMN number        INT   UNIQUE,
+    ADD COLUMN capacity      INT    ,
+    ADD COLUMN university_id BIGINT  REFERENCES university (id) ON DELETE CASCADE;
 
 ALTER TABLE faculties
-    ADD COLUMN name          varchar(256) NOT NULL,
-    ADD COLUMN university_id BIGINT       NOT NULL REFERENCES university (id) ON DELETE CASCADE;
+    ADD COLUMN name          varchar(256) UNIQUE,
+    ADD COLUMN university_id BIGINT        REFERENCES university (id) ON DELETE CASCADE;
 
 ALTER TABLE departments
-    ADD COLUMN name          varchar(256) NOT NULL,
-    ADD COLUMN faculty_id    BIGINT       NOT NULL REFERENCES faculties (id) ON DELETE CASCADE,
-    ADD COLUMN university_id BIGINT       NOT NULL REFERENCES university (id) ON DELETE CASCADE;
+    ADD COLUMN name          varchar(256) UNIQUE,
+    ADD COLUMN faculty_id    BIGINT        REFERENCES faculties (id) ON DELETE CASCADE,
+    ADD COLUMN university_id BIGINT        REFERENCES university (id) ON DELETE CASCADE;
 
 ALTER TABLE lectures
-    ADD COLUMN number      INT    NOT NULL,
-    ADD COLUMN date        DATE   NOT NULL,
-    ADD COLUMN audience_id BIGINT NOT NULL REFERENCES audiences (id) ON DELETE CASCADE,
-    ADD COLUMN lesson_id   BIGINT NOT NULL REFERENCES lessons (id) ON DELETE CASCADE,
-    ADD COLUMN teacher_id  BIGINT NOT NULL REFERENCES teachers (id) ON DELETE CASCADE,
-    ADD COLUMN schedule_id BIGINT NOT NULL REFERENCES schedule (id) ON DELETE CASCADE;
+    ADD COLUMN number      INT   ,
+    ADD COLUMN date        DATE   ,
+    ADD COLUMN audience_id BIGINT  REFERENCES audiences (id) ON DELETE SET NULL,
+    ADD COLUMN lesson_id   BIGINT  REFERENCES lessons (id) ON DELETE SET NULL,
+    ADD COLUMN teacher_id  BIGINT  REFERENCES teachers (id) ON DELETE SET NULL,
+    ADD COLUMN schedule_id BIGINT  REFERENCES schedule (id) ON DELETE CASCADE;
 
 ALTER TABLE groups
-    ADD COLUMN name          varchar(256) NOT NULL,
-    ADD COLUMN lecture_id    BIGINT       NOT NULL REFERENCES lectures (id) ON DELETE CASCADE,
-    ADD COLUMN department_id BIGINT       NOT NULL REFERENCES departments (id) ON DELETE CASCADE,
-    ADD COLUMN faculty_id    BIGINT       NOT NULL REFERENCES faculties (id) ON DELETE CASCADE,
-    ADD COLUMN university_id BIGINT       NOT NULL REFERENCES university (id) ON DELETE CASCADE;
+    ADD COLUMN name          varchar(256) UNIQUE,
+    ADD COLUMN lecture_id    BIGINT        REFERENCES lectures (id) ON DELETE SET NULL,
+    ADD COLUMN department_id BIGINT        REFERENCES departments (id) ON DELETE SET NULL,
+    ADD COLUMN faculty_id    BIGINT        REFERENCES faculties (id) ON DELETE CASCADE,
+    ADD COLUMN university_id BIGINT        REFERENCES university (id) ON DELETE CASCADE;
 
 ALTER TABLE students
-    ADD COLUMN first_name    varchar(50) NOT NULL,
-    ADD COLUMN last_name     varchar(50) NOT NULL,
-    ADD COLUMN middle_name   varchar(50) NOT NULL,
-    ADD COLUMN course_number INT         NOT NULL,
-    ADD COLUMN group_id      BIGINT      NOT NULL REFERENCES groups (id) ON DELETE CASCADE,
-    ADD COLUMN faculty_id    BIGINT      NOT NULL REFERENCES faculties (id) ON DELETE CASCADE,
-    ADD COLUMN university_id BIGINT      NOT NULL REFERENCES university (id) ON DELETE CASCADE;
+    ADD COLUMN first_name    varchar(50),
+    ADD COLUMN last_name     varchar(50),
+    ADD COLUMN middle_name   varchar(50),
+    ADD COLUMN course_number INT         ,
+    ADD COLUMN group_id      BIGINT      REFERENCES groups (id) ON DELETE SET NULL,
+    ADD COLUMN faculty_id    BIGINT      REFERENCES faculties (id) ON DELETE SET NULL,
+    ADD COLUMN university_id BIGINT       REFERENCES university (id) ON DELETE CASCADE;
 
 ALTER TABLE lessons
-    ADD COLUMN number     INT      NOT NULL,
-    ADD COLUMN start_time TIME     NOT NULL,
-    ADD COLUMN duration   INTERVAL NOT NULL,
-    ADD COLUMN subject_id BIGINT   NOT NULL REFERENCES subjects (id) ON DELETE CASCADE;
+    ADD COLUMN number     INT      ,
+    ADD COLUMN start_time TIME     ,
+    ADD COLUMN duration   INTERVAL,
+    ADD COLUMN subject_id BIGINT    REFERENCES subjects (id) ON DELETE SET NULL;
 
 ALTER TABLE subjects
-    ADD COLUMN name          varchar(256) NOT NULL,
-    ADD COLUMN student_id    BIGINT       NOT NULL REFERENCES students (id) ON DELETE CASCADE,
-    ADD COLUMN teacher_id    BIGINT       NOT NULL REFERENCES teachers (id) ON DELETE CASCADE,
-    ADD COLUMN university_id BIGINT       NOT NULL REFERENCES university (id) ON DELETE CASCADE;
+    ADD COLUMN name          varchar(256) UNIQUE,
+    ADD COLUMN university_id BIGINT        REFERENCES university (id) ON DELETE CASCADE;
 
 ALTER TABLE teachers
-    ADD COLUMN first_name    varchar(50) NOT NULL,
-    ADD COLUMN last_name     varchar(50) NOT NULL,
-    ADD COLUMN middle_name   varchar(50) NOT NULL,
-    ADD COLUMN student_id    BIGINT      NOT NULL REFERENCES students (id) ON DELETE CASCADE,
-    ADD COLUMN university_id BIGINT      NOT NULL REFERENCES university (id) ON DELETE CASCADE;
+    ADD COLUMN first_name    varchar(50) ,
+    ADD COLUMN last_name     varchar(50) ,
+    ADD COLUMN middle_name   varchar(50) ,
+    ADD COLUMN student_id    BIGINT       REFERENCES students (id) ON DELETE SET NULL,
+    ADD COLUMN university_id BIGINT       REFERENCES university (id) ON DELETE CASCADE;
