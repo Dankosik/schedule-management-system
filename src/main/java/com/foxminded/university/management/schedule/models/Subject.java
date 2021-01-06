2cfb1a4d0685c33ad1112c02c34c4e4430bb1838
dankos <daniil.koryto@gmail.com>
2021-01-06 21:43:08 +1000

    changed model Subject

--- src/main/java/com/foxminded/university/management/schedule/models/Subject.java
+++ src/main/java/com/foxminded/university/management/schedule/models/Subject.java
@@ -5,26 +5,15 @@ import java.util.Objects;
 public class Subject {
     private Long id;
     private String name;
-    private Long studentId;
-    private Long teacherId;
     private Long universityId;
 
     public Subject() {
 
     }
 
-    public Subject(Long id, String name, Long studentId, Long teacherId, Long universityId) {
+    public Subject(Long id, String name, Long universityId) {
         this.id = id;
         this.name = name;
-        this.studentId = studentId;
-        this.teacherId = teacherId;
-        this.universityId = universityId;
-    }
-
-    public Subject(String name, Long studentId, Long teacherId, Long universityId) {
-        this.name = name;
-        this.studentId = studentId;
-        this.teacherId = teacherId;
         this.universityId = universityId;
     }
 
@@ -44,22 +33,6 @@ public class Subject {
         this.name = name;
     }
 
-    public Long getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(Long studentId) {
-        this.studentId = studentId;
-    }
-
-    public Long getTeacherId() {
-        return teacherId;
-    }
-
-    public void setTeacherId(Long teacherId) {
-        this.teacherId = teacherId;
-    }
-
     public Long getUniversityId() {
         return universityId;
     }
@@ -73,14 +46,12 @@ public class Subject {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Subject subject = (Subject) o;
-        return Objects.equals(id, subject.id) && Objects.equals(name, subject.name) &&
-                Objects.equals(studentId, subject.studentId) && Objects.equals(teacherId, subject.teacherId) &&
-                Objects.equals(universityId, subject.universityId);
+        return Objects.equals(id, subject.id) && Objects.equals(name, subject.name) && Objects.equals(universityId, subject.universityId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, studentId, teacherId, universityId);
+        return Objects.hash(id, name, universityId);
     }
 
     @Override
@@ -88,8 +59,6 @@ public class Subject {
         return "Subject{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", studentId=" + studentId +
-                ", teacherId=" + teacherId +
                 ", universityId=" + universityId +
                 '}';
     }
