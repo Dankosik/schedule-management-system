f9b874714abebbc4fbc4f69078a969b489cbd262
dankos <daniil.koryto@gmail.com>
2021-01-06 22:27:10 +1000

    deleted field scheduleId from University

--- src/main/java/com/foxminded/university/management/schedule/models/University.java
+++ src/main/java/com/foxminded/university/management/schedule/models/University.java
@@ -4,18 +4,12 @@ import java.util.Objects;
 
 public class University {
     private Long id;
-    private Long scheduleId;
 
     public University() {
     }
 
-    public University(Long scheduleId) {
-        this.scheduleId = scheduleId;
-    }
-
-    public University(Long id, Long scheduleId) {
+    public University(Long id) {
         this.id = id;
-        this.scheduleId = scheduleId;
     }
 
     public Long getId() {
@@ -26,24 +20,23 @@ public class University {
         this.id = id;
     }
 
-    public Long getScheduleId() {
-        return scheduleId;
-    }
-
-    public void setScheduleId(Long scheduleId) {
-        this.scheduleId = scheduleId;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         University that = (University) o;
-        return Objects.equals(id, that.id) && Objects.equals(scheduleId, that.scheduleId);
+        return Objects.equals(id, that.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, scheduleId);
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "University{" +
+                "id=" + id +
+                '}';
     }
 }
