76d65be6badee237be2c21a429a60b652041047f
dankos <daniil.koryto@gmail.com>
2021-01-06 20:56:38 +1000

    added bean jdbcTemplate

--- src/main/java/com/foxminded/university/management/schedule/config/ApplicationConfig.java
+++ src/main/java/com/foxminded/university/management/schedule/config/ApplicationConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
@@ -24,5 +25,10 @@ public class ApplicationConfig {
         dataSource.setUsername(environment.getProperty("spring.datasource.username"));
         dataSource.setPassword(environment.getProperty("spring.datasource.password"));
         return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate(dataSource());
     }
 }
