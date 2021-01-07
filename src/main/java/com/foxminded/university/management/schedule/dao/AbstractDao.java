a2148147c9c50bb07cadffd793fa2b7e07f04fc6
dankos <daniil.koryto@gmail.com>
2021-01-07 21:08:57 +1000

    changed method save in AbstractDao

--- src/main/java/com/foxminded/university/management/schedule/dao/AbstractDao.java
+++ src/main/java/com/foxminded/university/management/schedule/dao/AbstractDao.java
@@ -1,19 +1,13 @@
 package com.foxminded.university.management.schedule.dao;
 
-import java.lang.reflect.InvocationTargetException;
-
-public abstract class AbstractDao<T> implements Dao<T> {
+public abstract class AbstractDao<T extends BaseEntity<Long>> implements Dao<T, Long> {
     @Override
     public T save(T object) {
-        try {
-            if (object.getClass().getMethod("getId").invoke(object) == null) return create(object);
-        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            e.printStackTrace();
-        }
-        return update(object);
+        return object.getId() == null ? create(object) : update(object);
     }
+
+    protected abstract T update(T object);
 
     protected abstract T create(T object);
 
-    protected abstract T update(T object);
 }
