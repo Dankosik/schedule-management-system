5783a9755601248e59f7b81eaa51e6039e171ac2
dankos <daniil.koryto@gmail.com>
2021-01-07 21:15:02 +1000

    merged table creation scripts into one file, other files deleted

--- src/main/resources/db/migration/V4__add_table_subjects_teachers.sql
+++
@@ -1,6 +0,0 @@
-create table subjects_teachers
-(
-    subject_id BIGINT REFERENCES subjects (id) ON DELETE CASCADE,
-    teacher_id BIGINT REFERENCES teachers (id) ON DELETE CASCADE,
-    PRIMARY KEY (subject_id, teacher_id)
-);
--- src/main/resources/db/migration/V5__add_table_subjects_students.sql
+++
@@ -1,6 +0,0 @@
-create table subjects_students
-(
-    subject_id BIGINT REFERENCES subjects (id) ON DELETE CASCADE,
-    student_id BIGINT REFERENCES students (id) ON DELETE CASCADE,
-    PRIMARY KEY (subject_id, student_id)
-);
