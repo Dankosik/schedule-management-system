497035470934ed75ff6882a4462388ef60b58458
dankos <daniil.koryto@gmail.com>
2021-01-07 21:08:33 +1000

    changed method from boolean delete(T object) to boolean deleteById(K id) in Dao

--- src/main/java/com/foxminded/university/management/schedule/dao/Dao.java
+++ src/main/java/com/foxminded/university/management/schedule/dao/Dao.java
@@ -3,14 +3,14 @@ package com.foxminded.university.management.schedule.dao;
 import java.util.List;
 import java.util.Optional;
 
-public interface Dao<T> {
-    Optional<T> getById(Long id);
+public interface Dao<T extends BaseEntity<K>, K> {
+    Optional<T> getById(K id);
 
     List<T> getAll();
 
     T save(T object);
 
-    boolean delete(T object);
+    boolean deleteById(K id);
 
     List<T> saveAll(List<T> objects);
 }
