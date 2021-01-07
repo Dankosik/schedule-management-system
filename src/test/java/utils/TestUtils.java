72633ce7bd35c02fc8bb9e0a2f08560288d842c4
dankos <daniil.koryto@gmail.com>
2021-01-08 04:59:41 +1000

    added TestUtils

---
+++ src/test/java/utils/TestUtils.java
@@ -0,0 +1,38 @@
+package utils;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class TestUtils {
+    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
+
+    public TestUtils(DataSource dataSource) {
+        this.dataSource = dataSource;
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public boolean existsById(String tableName, Long id) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT COUNT(*) from " + tableName + " WHERE id = ?;")) {
+            preparedStatement.setLong(1, id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                resultSet.next();
+                return resultSet.getLong(1) == 1;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public Map<String, Object> getEntry(String tableName, Long id) {
+        return jdbcTemplate.queryForMap("SELECT * from " + tableName + " WHERE id = " + id);
+    }
+}
