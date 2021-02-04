84b469e87228784c40c90ad0ff1e243e1064f4ab
dankos <daniil.koryto@gmail.com>
2021-02-05 02:50:25 +1000

    added SubjectController and templates subject.html, subjects.html

---
+++ src/main/java/com/foxminded/university/management/schedule/controllers/SubjectController.java
@@ -0,0 +1,28 @@
+package com.foxminded.university.management.schedule.controllers;
+
+import com.foxminded.university.management.schedule.service.SubjectService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class SubjectController {
+    private final SubjectService subjectService;
+
+    public SubjectController(SubjectService subjectService) {
+        this.subjectService = subjectService;
+    }
+
+    @GetMapping("/subjects")
+    public String showAllFaculties(Model model) {
+        model.addAttribute("subjects", subjectService.getAllSubjects());
+        return "subjects";
+    }
+
+    @GetMapping("/subjects/{id}")
+    public String showOneTeacher(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("subject", subjectService.getSubjectById(id));
+        return "subject";
+    }
+}
