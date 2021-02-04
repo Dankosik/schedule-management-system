ea126cd20613b842a5a32b07db363ee3ed60b6bd
dankos <daniil.koryto@gmail.com>
2021-02-05 02:49:11 +1000

    added LessonController and templates lesson.html

---
+++ src/main/java/com/foxminded/university/management/schedule/controllers/LessonController.java
@@ -0,0 +1,41 @@
+package com.foxminded.university.management.schedule.controllers;
+
+import com.foxminded.university.management.schedule.controllers.utils.StringUtils;
+import com.foxminded.university.management.schedule.models.Lesson;
+import com.foxminded.university.management.schedule.service.LessonService;
+import com.foxminded.university.management.schedule.service.SubjectService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class LessonController {
+    private final LessonService lessonService;
+    private final SubjectService subjectService;
+
+    public LessonController(LessonService lessonService, SubjectService subjectService) {
+        this.lessonService = lessonService;
+        this.subjectService = subjectService;
+    }
+
+    @GetMapping("/lessons")
+    public String showAllFaculties(Model model) {
+        List<Lesson> lessons = lessonService.getAllLessons();
+        model.addAttribute("lessons", lessons);
+
+        List<Duration> durations = lessonService.getDurationsForLessons(lessons);
+        List<String> formattedDurations = durations.stream()
+                .map(StringUtils::formatDuration)
+                .collect(Collectors.toList());
+
+        model.addAttribute("durations", formattedDurations);
+        model.addAttribute("subjectNames", subjectService.getSubjectNamesForLessons(lessons));
+        model.addAttribute("subjects", subjectService.getSubjectsForLessons(lessons));
+        return "lessons";
+    }
+
+}
