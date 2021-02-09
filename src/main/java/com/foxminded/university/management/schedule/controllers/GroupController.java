d0be4ea5747d133c48efeb7008c5d1245671c5dc
dankos <daniil.koryto@gmail.com>
2021-02-10 02:50:24 +1000

    added table students for each group

--- src/main/java/com/foxminded/university/management/schedule/controllers/GroupController.java
+++ src/main/java/com/foxminded/university/management/schedule/controllers/GroupController.java
@@ -1,6 +1,8 @@
 package com.foxminded.university.management.schedule.controllers;
 
+import com.foxminded.university.management.schedule.models.Group;
 import com.foxminded.university.management.schedule.service.impl.GroupServiceImpl;
+import com.foxminded.university.management.schedule.service.impl.StudentServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class GroupController {
     private final GroupServiceImpl groupService;
+    private final StudentServiceImpl studentService;
 
-    public GroupController(GroupServiceImpl groupService) {
+    public GroupController(GroupServiceImpl groupService, StudentServiceImpl studentService) {
         this.groupService = groupService;
+        this.studentService = studentService;
     }
 
     @GetMapping("/groups")
@@ -22,7 +26,9 @@ public class GroupController {
 
     @GetMapping("/groups/{id}")
     public String showOneGroup(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("group", groupService.getGroupById(id));
+        Group group = groupService.getGroupById(id);
+        model.addAttribute("group", group);
+        model.addAttribute("students", studentService.getStudentsForGroup(group));
         return "group";
     }
 }
