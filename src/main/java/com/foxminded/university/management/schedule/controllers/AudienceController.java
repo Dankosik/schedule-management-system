1baed655d2d3cb6339f2d123750913160827d9e8
dankos <daniil.koryto@gmail.com>
2021-02-12 02:26:00 +1000

    added lectures table in html in audience page in AudienceController

--- src/main/java/com/foxminded/university/management/schedule/controllers/AudienceController.java
+++ src/main/java/com/foxminded/university/management/schedule/controllers/AudienceController.java
@@ -1,17 +1,35 @@
 package com.foxminded.university.management.schedule.controllers;
 
-import com.foxminded.university.management.schedule.service.impl.AudienceServiceImpl;
+import com.foxminded.university.management.schedule.controllers.utils.StringUtils;
+import com.foxminded.university.management.schedule.models.*;
+import com.foxminded.university.management.schedule.service.impl.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class AudienceController {
     private final AudienceServiceImpl audienceService;
+    private final LectureServiceImpl lectureService;
+    private final LessonServiceImpl lessonService;
+    private final SubjectServiceImpl subjectService;
+    private final TeacherServiceImpl teacherService;
+    private final StudentServiceImpl studentService;
+    private final GroupServiceImpl groupService;
 
-    public AudienceController(AudienceServiceImpl audienceService) {
+    public AudienceController(AudienceServiceImpl audienceService, LectureServiceImpl lectureService, LessonServiceImpl lessonService, SubjectServiceImpl subjectService, TeacherServiceImpl teacherService, StudentServiceImpl studentService, GroupServiceImpl groupService) {
         this.audienceService = audienceService;
+        this.lectureService = lectureService;
+        this.lessonService = lessonService;
+        this.subjectService = subjectService;
+        this.teacherService = teacherService;
+        this.studentService = studentService;
+        this.groupService = groupService;
     }
 
     @GetMapping("/audiences")
@@ -22,7 +40,30 @@ public class AudienceController {
 
     @GetMapping("/audiences/{id}")
     public String showOneAudience(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("audience", audienceService.getAudienceById(id));
+        Audience audience = audienceService.getAudienceById(id);
+        model.addAttribute("audience", audience);
+        List<Lecture> lectures = lectureService.getLecturesForAudience(audience);
+        model.addAttribute("lectures", lectures);
+
+        List<Lesson> lessons = lessonService.getLessonsForLectures(lectures);
+        List<Duration> durations = lessonService.getDurationsForLessons(lessons);
+        List<String> formattedDurations = durations.stream()
+                .map(StringUtils::formatDurationInMinutes)
+                .collect(Collectors.toList());
+
+        model.addAttribute("durations", formattedDurations);
+        model.addAttribute("subjectNames", subjectService.getSubjectNamesForLessons(lessons));
+        model.addAttribute("startTimes", lessonService.getStartTimesForLessons(lessons));
+
+        List<Teacher> teachers = teacherService.getTeachersForLectures(lectures);
+        model.addAttribute("teachers", teachers);
+        model.addAttribute("teacherNames", teacherService.getLastNamesWithInitialsForTeachers(teachers));
+
+        model.addAttribute("subjects", subjectService.getSubjectsForLectures(lectures));
+
+        List<Student> students = studentService.getAllStudents();
+        model.addAttribute("groupNames", groupService.getGroupNamesForStudents(students));
+        model.addAttribute("groups", groupService.getGroupsForStudents(students));
         return "audience";
     }
 }
