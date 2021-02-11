0dc1c3a9a17a5d2f90e04f553b5fe794ba3b196d
dankos <daniil.koryto@gmail.com>
2021-02-12 03:29:43 +1000

    changed test for show one teacher for TeacherController

--- src/test/java/com/foxminded/university/management/schedule/controllers/TeacherControllerTest.java
+++ src/test/java/com/foxminded/university/management/schedule/controllers/TeacherControllerTest.java
@@ -1,9 +1,7 @@
 package com.foxminded.university.management.schedule.controllers;
 
-import com.foxminded.university.management.schedule.models.Faculty;
-import com.foxminded.university.management.schedule.models.Teacher;
-import com.foxminded.university.management.schedule.service.impl.FacultyServiceImpl;
-import com.foxminded.university.management.schedule.service.impl.TeacherServiceImpl;
+import com.foxminded.university.management.schedule.models.*;
+import com.foxminded.university.management.schedule.service.impl.*;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +10,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
@@ -24,9 +27,19 @@ class TeacherControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @MockBean
+    private FacultyServiceImpl facultyService;
+    @MockBean
+    private LectureServiceImpl lectureService;
+    @MockBean
+    private AudienceServiceImpl audienceService;
+    @MockBean
+    private LessonServiceImpl lessonService;
+    @MockBean
     private TeacherServiceImpl teacherService;
     @MockBean
-    private FacultyServiceImpl facultyService;
+    private SubjectServiceImpl subjectService;
+    @MockBean
+    private GroupServiceImpl groupService;
 
     @Test
     public void shouldReturnViewWithAllTeachers() throws Exception {
@@ -60,9 +73,60 @@ class TeacherControllerTest {
         Faculty faculty = new Faculty(1L, "FAIT", 1L);
         when(facultyService.getFacultyById(1L)).thenReturn(faculty);
 
+        List<Lecture> lectures = List.of(
+                new Lecture(1L, 1, Date.valueOf(LocalDate.of(2021, 1, 1)), 1L, 1L, 1L, 1L),
+                new Lecture(2L, 2, Date.valueOf(LocalDate.of(2021, 1, 1)), 2L, 2L, 2L, 1L));
+        when(lectureService.getLecturesForTeacher(teacher)).thenReturn(lectures);
+
+        List<Lesson> lessons = List.of(
+                new Lesson(1L, 1, Time.valueOf(LocalTime.of(8, 30, 0)), Duration.ofMinutes(90), 1L),
+                new Lesson(2L, 2, Time.valueOf(LocalTime.of(10, 10, 0)), Duration.ofMinutes(90), 2L));
+        when(lessonService.getLessonsForLectures(lectures)).thenReturn(lessons);
+
+        List<Duration> durations = List.of(Duration.ofMinutes(90), Duration.ofMinutes(90));
+        when(lessonService.getDurationsForLessons(lessons)).thenReturn(durations);
+        List<String> formattedDurations = List.of("1:30:00", "1:30:00");
+
+        List<String> subjectNames = List.of("Math", "Art");
+        when(subjectService.getSubjectNamesForLessons(lessons)).thenReturn(subjectNames);
+
+        List<Time> startTimes = List.of(
+                Time.valueOf(LocalTime.of(8, 30, 0)),
+                Time.valueOf(LocalTime.of(10, 10, 0)));
+        when(lessonService.getStartTimesForLessons(lessons)).thenReturn(startTimes);
+
+        List<Audience> audiences = List.of(
+                new Audience(1L, 301, 45, 1L),
+                new Audience(2L, 302, 55, 1L));
+        when(audienceService.getAudiencesForLectures(lectures)).thenReturn(audiences);
+
+        List<Integer> audienceNumbers = List.of(301, 302);
+        when(audienceService.getAudienceNumbersForAudiences(audiences)).thenReturn(audienceNumbers);
+
+        List<Subject> subjects = List.of(
+                new Subject(1L, "Math", 1L),
+                new Subject(2L, "Art", 1L));
+        when(subjectService.getSubjectsForLectures(lectures)).thenReturn(subjects);
+
+        List<Group> groups = List.of(
+                new Group(1L, "AB-01", 1L, 1L),
+                new Group(2L, "AB-11", 1L, 1L));
+        when(groupService.getGroupsForLectures(lectures)).thenReturn(groups);
+
+        List<String> groupNames = List.of("AB-01", "AB-11");
+        when(groupService.getGroupNamesForLectures(lectures)).thenReturn(groupNames);
+
         mockMvc.perform(get("/teachers/{id}", 1))
                 .andExpect(status().isOk())
                 .andExpect(view().name("teacher"))
+                .andExpect(model().attribute("lectures", lectures))
+                .andExpect(model().attribute("durations", formattedDurations))
+                .andExpect(model().attribute("subjectNames", subjectNames))
+                .andExpect(model().attribute("startTimes", startTimes))
+                .andExpect(model().attribute("audienceNumbers", audienceNumbers))
+                .andExpect(model().attribute("subjects", subjects))
+                .andExpect(model().attribute("groups", groups))
+                .andExpect(model().attribute("groupNames", groupNames))
                 .andExpect(model().attribute("teacher", teacher))
                 .andExpect(model().attribute("faculty", faculty));
     }
