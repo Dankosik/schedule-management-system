979e5b103bddc6b45bc9e08e968b34dd010966a1
dankos <daniil.koryto@gmail.com>
2021-02-12 02:45:04 +1000

    added students and lectures tables in view that show one group in GroupController

--- src/main/java/com/foxminded/university/management/schedule/controllers/GroupController.java
+++ src/main/java/com/foxminded/university/management/schedule/controllers/GroupController.java
@@ -1,21 +1,37 @@
 package com.foxminded.university.management.schedule.controllers;
 
-import com.foxminded.university.management.schedule.models.Group;
-import com.foxminded.university.management.schedule.service.impl.GroupServiceImpl;
-import com.foxminded.university.management.schedule.service.impl.StudentServiceImpl;
+import com.foxminded.university.management.schedule.controllers.utils.StringUtils;
+import com.foxminded.university.management.schedule.models.*;
+import com.foxminded.university.management.schedule.service.impl.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class GroupController {
     private final GroupServiceImpl groupService;
     private final StudentServiceImpl studentService;
+    private final LectureServiceImpl lectureService;
+    private final LessonServiceImpl lessonService;
+    private final SubjectServiceImpl subjectService;
+    private final AudienceServiceImpl audienceService;
+    private final TeacherServiceImpl teacherService;
 
-    public GroupController(GroupServiceImpl groupService, StudentServiceImpl studentService) {
+    public GroupController(GroupServiceImpl groupService, StudentServiceImpl studentService,
+                           LectureServiceImpl lectureService, LessonServiceImpl lessonService,
+                           SubjectServiceImpl subjectService, AudienceServiceImpl audienceService, TeacherServiceImpl teacherService) {
         this.groupService = groupService;
         this.studentService = studentService;
+        this.lectureService = lectureService;
+        this.lessonService = lessonService;
+        this.subjectService = subjectService;
+        this.audienceService = audienceService;
+        this.teacherService = teacherService;
     }
 
     @GetMapping("/groups")
@@ -29,6 +45,29 @@ public class GroupController {
         Group group = groupService.getGroupById(id);
         model.addAttribute("group", group);
         model.addAttribute("students", studentService.getStudentsForGroup(group));
+
+        List<Lecture> lectures = lectureService.getLecturesForGroup(group);
+        model.addAttribute("lectures", lectures);
+
+        List<Lesson> lessons = lessonService.getLessonsForLectures(lectures);
+        List<Duration> durations = lessonService.getDurationsForLessons(lessons);
+        List<String> formattedDurations = durations.stream()
+                .map(StringUtils::formatDurationInMinutes)
+                .collect(Collectors.toList());
+
+        model.addAttribute("durations", formattedDurations);
+        model.addAttribute("subjectNames", subjectService.getSubjectNamesForLessons(lessons));
+        model.addAttribute("startTimes", lessonService.getStartTimesForLessons(lessons));
+
+        model.addAttribute("subjects", subjectService.getSubjectsForLectures(lectures));
+
+        List<Teacher> teachers = teacherService.getTeachersForLectures(lectures);
+        model.addAttribute("teachers", teachers);
+        model.addAttribute("teacherNames", teacherService.getLastNamesWithInitialsForTeachers(teachers));
+
+        List<Audience> audiences = audienceService.getAudiencesForLectures(lectures);
+        model.addAttribute("audiences", audiences);
+        model.addAttribute("audienceNumbers", audienceService.getAudienceNumbersForAudiences(audiences));
         return "group";
     }
 }
