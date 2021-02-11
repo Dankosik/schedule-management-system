0403e00ef7fa51031162a97539afae4015198f9f
dankos <daniil.koryto@gmail.com>
2021-02-12 02:44:00 +1000

    changed test for FacultyController for method that returned html that one faculty in FacultyController

--- src/test/java/com/foxminded/university/management/schedule/controllers/FacultyControllerTest.java
+++ src/test/java/com/foxminded/university/management/schedule/controllers/FacultyControllerTest.java
@@ -1,7 +1,11 @@
 package com.foxminded.university.management.schedule.controllers;
 
 import com.foxminded.university.management.schedule.models.Faculty;
+import com.foxminded.university.management.schedule.models.Group;
+import com.foxminded.university.management.schedule.models.Teacher;
 import com.foxminded.university.management.schedule.service.impl.FacultyServiceImpl;
+import com.foxminded.university.management.schedule.service.impl.GroupServiceImpl;
+import com.foxminded.university.management.schedule.service.impl.TeacherServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +27,10 @@ class FacultyControllerTest {
     private MockMvc mockMvc;
     @MockBean
     private FacultyServiceImpl facultyService;
+    @MockBean
+    private GroupServiceImpl groupService;
+    @MockBean
+    private TeacherServiceImpl teacherService;
 
     @Test
     public void shouldReturnViewWithAllFaculties() throws Exception {
@@ -43,9 +51,21 @@ class FacultyControllerTest {
         Faculty faculty = new Faculty(1L, "FAIT", 1L);
         when(facultyService.getFacultyById(1L)).thenReturn(faculty);
 
+        List<Group> groups = List.of(
+                new Group(1L, "AG-01", 1L, 1L),
+                new Group(2L, "GD-02", 1L, 1L));
+        when(groupService.getGroupsForFaculty(faculty)).thenReturn(groups);
+
+        List<Teacher> teachers = List.of(
+                new Teacher("John", "Jackson", "Jackson", 1L, 1L),
+                new Teacher("Mike", "Conor", "Conor", 1L, 1L));
+        when(teacherService.getTeachersForFaculty(faculty)).thenReturn(teachers);
+
         mockMvc.perform(get("/faculties/{id}", 1))
                 .andExpect(status().isOk())
                 .andExpect(view().name("faculty"))
+                .andExpect(model().attribute("groups", groups))
+                .andExpect(model().attribute("teachers", teachers))
                 .andExpect(model().attribute("faculty", faculty));
     }
 }
