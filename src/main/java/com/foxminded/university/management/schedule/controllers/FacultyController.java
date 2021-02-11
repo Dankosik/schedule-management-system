c78382616cd039c3ab6299e92b49aab150891b03
dankos <daniil.koryto@gmail.com>
2021-02-12 02:42:58 +1000

    added groups and teachers tables in view that show 0ne faculty in FacultyController

--- src/main/java/com/foxminded/university/management/schedule/controllers/FacultyController.java
+++ src/main/java/com/foxminded/university/management/schedule/controllers/FacultyController.java
@@ -1,17 +1,28 @@
 package com.foxminded.university.management.schedule.controllers;
 
+import com.foxminded.university.management.schedule.models.Faculty;
+import com.foxminded.university.management.schedule.models.Group;
 import com.foxminded.university.management.schedule.service.impl.FacultyServiceImpl;
+import com.foxminded.university.management.schedule.service.impl.GroupServiceImpl;
+import com.foxminded.university.management.schedule.service.impl.TeacherServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 public class FacultyController {
     private final FacultyServiceImpl facultyService;
+    private final GroupServiceImpl groupService;
+    private final TeacherServiceImpl teacherService;
 
-    public FacultyController(FacultyServiceImpl facultyService) {
+    public FacultyController(FacultyServiceImpl facultyService, GroupServiceImpl groupService,
+                             TeacherServiceImpl teacherService) {
         this.facultyService = facultyService;
+        this.groupService = groupService;
+        this.teacherService = teacherService;
     }
 
     @GetMapping("/faculties")
@@ -22,7 +33,11 @@ public class FacultyController {
 
     @GetMapping("/faculties/{id}")
     public String showOneFaculty(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("faculty", facultyService.getFacultyById(id));
+        Faculty faculty = facultyService.getFacultyById(id);
+        model.addAttribute("faculty", faculty);
+
+        model.addAttribute("groups", groupService.getGroupsForFaculty(faculty));
+        model.addAttribute("teachers", teacherService.getTeachersForFaculty(faculty));
         return "faculty";
     }
 }
