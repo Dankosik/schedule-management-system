404582754e08bceee50eb110c5ffaf83e7c5abbf
dankos <daniil.koryto@gmail.com>
2021-02-21 19:46:26 +1000

    added StringUtils with tests

---
+++ src/main/java/com/foxminded/university/management/schedule/controllers/utils/StringUtils.java
@@ -0,0 +1,16 @@
+package com.foxminded.university.management.schedule.controllers.utils;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class StringUtils {
+    private static final DurationFormatter durationFormatter = new DurationFormatter();
+
+    public static List<String> formatListOfDurations(List<Duration> durations) {
+        return durations.stream()
+                .map(duration -> durationFormatter.print(duration, Locale.getDefault()))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/com/foxminded/university/management/schedule/controllers/utils/StringUtilsTest.java
@@ -0,0 +1,34 @@
+package com.foxminded.university.management.schedule.controllers.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+class StringUtilsTest {
+    @MockBean
+    private DurationFormatter durationFormatter;
+    private final List<Duration> durations = List.of(Duration.ofMinutes(90), Duration.ofMinutes(90), Duration.ofMinutes(90), Duration.ofMinutes(80));
+
+    @Test
+    public void shouldReturnFormattedListOfDurations() {
+        when(durationFormatter.print(Duration.ofMinutes(90), Locale.getDefault())).thenReturn("1:30");
+        when(durationFormatter.print(Duration.ofMinutes(80), Locale.getDefault())).thenReturn("1:20");
+
+        List<String> expected = List.of("1:30", "1:30", "1:30", "1:20");
+        List<String> actual = StringUtils.formatListOfDurations(durations);
+
+        assertEquals(expected, actual);
+    }
+
+}
