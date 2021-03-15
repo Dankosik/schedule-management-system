a6504fcdffe7b93e1b18aafd8a7051ffebfcad2e
dankos <daniil.koryto@gmail.com>
2021-03-16 03:33:16 +1000

    changed Faculty, Group to Entity

--- src/main/java/com/foxminded/university/management/schedule/models/Faculty.java
+++ src/main/java/com/foxminded/university/management/schedule/models/Faculty.java
@@ -2,25 +2,36 @@ package com.foxminded.university.management.schedule.models;
 
 import com.foxminded.university.management.schedule.dao.BaseEntity;
 
+import javax.persistence.*;
+import java.util.List;
 import java.util.Objects;
 
+@Entity
+@Table(name = "faculties")
 public class Faculty implements BaseEntity<Long> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "faculty")
+    private List<Group> groups;
+
+    public Faculty(Long id, String name, List<Group> groups) {
+        this.id = id;
+        this.name = name;
+        this.groups = groups;
+    }
+
+    public Faculty(String name, List<Group> groups) {
+        this.name = name;
+        this.groups = groups;
+    }
 
     public Faculty() {
 
     }
 
-    public Faculty(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Faculty(String name) {
-        this.name = name;
-    }
-
+    @Override
     public Long getId() {
         return id;
     }
@@ -37,17 +48,25 @@ public class Faculty implements BaseEntity<Long> {
         this.name = name;
     }
 
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Faculty faculty = (Faculty) o;
-        return Objects.equals(name, faculty.name);
+        return Objects.equals(name, faculty.name) && Objects.equals(groups, faculty.groups);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(name, groups);
     }
 
     @Override
@@ -55,6 +74,7 @@ public class Faculty implements BaseEntity<Long> {
         return "Faculty{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", groups=" + groups +
                 '}';
     }
 }
--- src/main/java/com/foxminded/university/management/schedule/models/Group.java
+++ src/main/java/com/foxminded/university/management/schedule/models/Group.java
@@ -2,41 +2,40 @@ package com.foxminded.university.management.schedule.models;
 
 import com.foxminded.university.management.schedule.dao.BaseEntity;
 
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity
+@Table(name = "groups")
 public class Group implements BaseEntity<Long> {
+    @Id
     private Long id;
     private String name;
-    private Long facultyId;
+    @ManyToOne
+    @JoinColumn(name="faculty_id", nullable=false)
+    private Faculty faculty;
 
     public Group() {
     }
 
-    public Group(Long id, String name, Long facultyId) {
+    public Group(Long id, String name, Faculty faculty) {
         this.id = id;
         this.name = name;
-        this.facultyId = facultyId;
+        this.faculty = faculty;
     }
 
-    public Group(String name, Long facultyId) {
+    public Group(String name, Faculty faculty) {
         this.name = name;
-        this.facultyId = facultyId;
+        this.faculty = faculty;
     }
 
+    @Override
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public Long getFacultyId() {
-        return facultyId;
-    }
-
-    public void setFacultyId(Long facultyId) {
-        this.facultyId = facultyId;
     }
 
     public String getName() {
@@ -47,25 +46,33 @@ public class Group implements BaseEntity<Long> {
         this.name = name;
     }
 
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Group group = (Group) o;
-        return Objects.equals(name, group.name) && Objects.equals(facultyId, group.facultyId);
+        return Objects.equals(name, group.name) && Objects.equals(faculty, group.faculty);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, facultyId);
+        return Objects.hash(name, faculty);
     }
 
     @Override
     public String toString() {
         return "Group{" +
                 "id=" + id +
-                ", name='" + name +
-                ", facultyId=" + facultyId +
+                ", name='" + name + '\'' +
+                ", faculty=" + faculty +
                 '}';
     }
 }
